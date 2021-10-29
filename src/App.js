b21a5efef430ce679f137ce7d007812ab1e544c0
abbassiriyus <86982819+abbassiriyus@users.noreply.github.com>
2021-10-29 13:54:51 +0500

    src papaka joylandi
    djjdj

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Dashboard from './pages/Dashboard';
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'antd/dist/antd.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+     
+<Switch>
+  <Route path="/">
+  <Dashboard/>
+  </Route>
+</Switch>
+      
+        </BrowserRouter>
+      </div>
+    )
+  }
 }
-
-export default App;
