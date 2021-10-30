8358ee47897daab79bbd69dd629112de15181399
abbassiriyus <86982819+abbassiriyus@users.noreply.github.com>
2021-10-30 11:33:28 +0500

    boshlashga tayyor
    sdf

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'antd/dist/antd.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Gallery from './pages/Gallery';
+import Yangiliklar from './pages/Yangiliklar';
 
 export default class App extends Component {
   render() {
@@ -12,8 +14,14 @@ export default class App extends Component {
         <BrowserRouter>
      
 <Switch>
-  <Route path="/">
+<Route exact path="/">
   <Dashboard/>
+  </Route>
+  <Route exact path="/gallery">
+  <Gallery/>
+  </Route>
+  <Route exact path="/yangiliklar">
+  <Yangiliklar/>
   </Route>
 </Switch>
       
